8b5eff571ad326507d5dbc21e1b435662638d4f7
Sebastian Glaser <anx@ulzq.de>
2020-03-19 10:20:19 +0100

    fix: seed records

--- seed/seed_db.js
+++ seed/seed_db.js
@@ -1,10 +1,11 @@
 var faker = require("faker");
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const Record = require("../models/Record");
 
 console.log("I shall seed");
 
-(async function() {
+( async function() {
   /**CONNECT TO DB */
   mongoose.connect("mongodb://localhost:27017/record-shop", {
     useNewUrlParser: true,
@@ -46,5 +47,39 @@ console.log("I shall seed");
     console.error(e);
   }
 
-  mongoose.connection.close();
+  // Records
+
+  console.log("I will purge all the old records...");
+
+  try {
+    await Record.deleteMany({});
+    console.log("Records purged");
+  } catch (err) {
+    console.error(err);
+  }
+
+  const recordPromises = Array(10)
+    .fill(null)
+    .map(() => {
+      const record = new Record({
+        title:  faker.name.firstName(),
+        artist: faker.name.lastName(),
+        year:   faker.random.number(),
+        img:    faker.random.image(),
+        price:  faker.commerce.price()
+      });
+      return record.save();
+    });
+
+  try {
+    await Promise.all(userPromises);
+    console.log("Records seeded");
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    mongoose.connection.close();
+  }
+  catch(e){
+  }
 })();
